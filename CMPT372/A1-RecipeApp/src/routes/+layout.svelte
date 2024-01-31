52161208dcc943932db108bcee94904281010d29
Jai <malhi.jaivir@gmail.com>
2024-01-30 23:05:51 -0800

    UPDATE: added basic skeleton components & set color theme

--- CMPT372/A1-RecipeApp/src/routes/+layout.svelte
+++ CMPT372/A1-RecipeApp/src/routes/+layout.svelte
@@ -1,5 +1,19 @@
 <script lang="ts">
+	import { AppShell, AppBar, Avatar } from '@skeletonlabs/skeleton';
 	import '../app.postcss';
 </script>
 
-<slot />
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">Recipe App</svelte:fragment>
+			<svelte:fragment slot="trail"
+				><Avatar initials="JM" background="bg-primary-500" width="w-12" /></svelte:fragment
+			>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+</AppShell>
