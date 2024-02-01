b9d34a6824df7eb08e9582d0f0b68a6408466894
Jai <malhi.jaivir@gmail.com>
2024-02-01 02:54:11 -0800

    FEAT: recipe display
    recipe display added alongside features such as dateFormatter, deletion confirmation & functionality, & limiting the ingredient display to 4 elements

--- CMPT372/A1-RecipeApp/src/routes/recipes/+layout.svelte
+++ CMPT372/A1-RecipeApp/src/routes/recipes/+layout.svelte
@@ -4,8 +4,8 @@
 
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 	import { initializeStores } from '@skeletonlabs/skeleton';
-	import { Toast, getToastStore } from '@skeletonlabs/skeleton';
-	import type { ToastSettings, ToastStore } from '@skeletonlabs/skeleton';
+	import { Toast } from '@skeletonlabs/skeleton';
+	import { Modal } from '@skeletonlabs/skeleton';
 	import { goto } from '$app/navigation';
 
 	initializeStores();
@@ -20,17 +20,23 @@
 	<meta name="description" content="Simple recipe app created with sveltekit & skeleton" />
 </svelte:head>
 
-<Toast position="tr" />
+<Toast position="br" />
+<Modal />
 
 <AppShell>
 	<svelte:fragment slot="header">
-		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+		<AppBar
+			gridColumns="grid-cols-3"
+			slotDefault="place-self-center"
+			slotTrail="place-content-end"
+			border="border-b-2 border-surface-600"
+		>
 			<svelte:fragment slot="lead">
 				<a href="/recipes" class="h2 hover:text-primary-500 font-medium">
 					<i class="fa-solid fa-house text-4xl"></i>
 				</a>
 			</svelte:fragment>
-			<h1 class="text-4xl">Recipe Safe</h1>
+			<h1 class="text-4xl text-center">Recipe Safe</h1>
 			<svelte:fragment slot="trail">
 				<button
 					on:click={() => {
@@ -43,15 +49,30 @@
 		</AppBar>
 	</svelte:fragment>
 	<!-- Router Slot -->
-	<div class="container p-10 mx-auto">
+	<div class="p-7 mx-auto">
 		<slot />
 	</div>
 	<!-- ---- / ---- -->
 	<svelte:fragment slot="footer">
-		<AppBar gridColumns="grid-cols-1" slotDefault="place-self-center">
-			<a class="anchor" href="https://kit.svelte.dev/">Sveltekit</a>
-			+
-			<a class="anchor" href="https://www.skeleton.dev/">Skeleton</a>
+		<AppBar
+			gridColumns="grid-cols-3"
+			slotDefault="place-self-center"
+			slotTrail="place-content-end"
+			border="border-t-2 border-surface-600"
+		>
+			<svelte:fragment slot="lead">
+				<div>
+					<a class="anchor" href="https://kit.svelte.dev/">Sveltekit</a>
+					+
+					<a class="anchor" href="https://www.skeleton.dev/">Skeleton</a>
+				</div>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="anchor"
+					href="https://github.com/jaimalhi/Academics/tree/main/CMPT372/A1-RecipeApp">GitHub</a
+				>
+			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
 </AppShell>
