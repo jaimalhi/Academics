faa272925d399374a86709876442e387b6d475d8
Jai <malhi.jaivir@gmail.com>
2024-01-31 22:13:26 -0800

    FEAT: default +layout created including header/nav & footer

--- CMPT372/A1-RecipeApp/src/routes/+layout.svelte
+++ CMPT372/A1-RecipeApp/src/routes/+layout.svelte
@@ -1,19 +1,39 @@
 <script lang="ts">
-	import { AppShell, AppBar, Avatar } from '@skeletonlabs/skeleton';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 	import '../app.postcss';
 </script>
 
+<svelte:head>
+	<title>Sveltekit Recipe App</title>
+	<meta name="description" content="Simple recipe app created with sveltekit & skeleton" />
+</svelte:head>
+
 <AppShell>
 	<svelte:fragment slot="header">
-		<AppBar>
-			<svelte:fragment slot="lead">Recipe App</svelte:fragment>
-			<svelte:fragment slot="trail"
-				><Avatar initials="JM" background="bg-primary-500" width="w-12" /></svelte:fragment
-			>
+		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+			<svelte:fragment slot="lead">
+				<a href="/" class="h2 hover:text-primary-500 font-medium">
+					<i class="fa-solid fa-house text-4xl"></i>
+				</a>
+			</svelte:fragment>
+			<h1 class="text-4xl">Recipe App</h1>
+			<svelte:fragment slot="trail">
+				<a href="/add" class="btn variant-filled hover:bg-primary-500 hover:text-white"
+					>Add Recipe</a
+				>
+			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
 	<!-- Router Slot -->
-	<slot />
+	<div class="container p-10 mx-auto">
+		<slot />
+	</div>
 	<!-- ---- / ---- -->
-	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<svelte:fragment slot="footer">
+		<AppBar gridColumns="grid-cols-1" slotDefault="place-self-center">
+			<a class="anchor" href="https://kit.svelte.dev/">Sveltekit</a>
+			+
+			<a class="anchor" href="https://www.skeleton.dev/">Skeleton</a>
+		</AppBar>
+	</svelte:fragment>
 </AppShell>
--- CMPT372/A1-RecipeApp/src/routes/+page.svelte
+++ CMPT372/A1-RecipeApp/src/routes/+page.svelte
@@ -1,5 +1,3 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
-
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="block card p-4 bg-gradient-to-br variant-gradient-success-error">Basic</div>
 </div>
