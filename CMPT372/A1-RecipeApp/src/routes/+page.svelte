4bb04c03be9eb0cd862faa08278fb8bd0c31d646
Jai <malhi.jaivir@gmail.com>
2024-01-31 22:32:02 -0800

    FEAT: added error.svelte to handle basic routing errors

--- CMPT372/A1-RecipeApp/src/routes/+page.svelte
+++ CMPT372/A1-RecipeApp/src/routes/+page.svelte
@@ -1,3 +1,3 @@
-<div class="container h-full mx-auto flex justify-center items-center">
+<div class="h-full flex justify-center items-center">
 	<div class="block card p-4 bg-gradient-to-br variant-gradient-success-error">Basic</div>
 </div>
